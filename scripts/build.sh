a1b4721abb7d324bacfc2365b7c6caa9fa3615d2
Dhuan Oliveira <dhuanco@gmail.com>
2024-07-26 21:30:34 +0200

    Statically link libyaml

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
-set -e
+set -ex
 gcc -I libyaml/include -o assert.o -c assert.c
 gcc -I libyaml/include -o config.o -c config.c
 gcc -I libyaml/include -o options.o -c options.c
 gcc -I libyaml/include -o common.o -c common.c
-gcc -L libyaml/src/.libs -l yaml -g -o ./dist/assert assert.o config.o options.o common.o
+gcc -o ./dist/assert assert.o config.o options.o common.o libyaml/src/.libs/libyaml.a
